3b526d04617823cdbff2593eb2e47a62472faf4c
Nhat Nguyen <natt.lefevre@gmail.com>
2019-09-28 16:17:50 -0700

    :100: Pie graph title and colors

--- src/javascript/vote.js
+++ src/javascript/vote.js
@@ -4,10 +4,17 @@ var ctx = document.getElementById('graph').getContext('2d');
 var chart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ['Red', 'Blue'],
+    labels: ["Yes", "No"],
     datasets: [{
-      data: [10, 45],
+      label: "Population (millions)",
+      backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+      data: [2478, 5267]
     }]
   },
-  options: {}
+  options: {
+    title: {
+      display: true,
+      text: 'Should we go left or right?'
+    }
+  }
 });
