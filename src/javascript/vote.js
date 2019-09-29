5226f19175b47180ccb11e7a6585ebcc5df78a4c
Hassan Syyid <hassansyyid@gmail.com>
2019-09-29 06:58:59 -0700

    ugh

--- src/javascript/vote.js
+++ src/javascript/vote.js
@@ -1,5 +1,4 @@
 import Chart from 'chart.js';
-import {votes} from './api.js';
 
 var ctx = document.getElementById('graph').getContext('2d');
 var chart = new Chart(ctx, {
@@ -21,8 +20,28 @@ var chart = new Chart(ctx, {
   }
 });
 
+// Fetch every 1000 milliseconds
 setInterval(() => {
-    chart.update();
-}, 1000)
+    fetch(`http://127.0.0.1:5000/votes`, { method: "GET" })
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        let { data } = response;
+        let votes = data
+        chart.config.data = {
+            labels: votes.labels,
+            datasets: [{
+              backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+              data: votes.votes,
+            }]
+          }
+        chart.update();
+        console.error(votes)
+      })
+      .catch(err => {
+        console.error(err);
+      })
+}, 1000);
 
 console.log(votes);
