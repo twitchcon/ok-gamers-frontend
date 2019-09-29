1c938f2be766f30928c3cd723a0b24d2f1bb1567
Hassan Syyid <hassansyyid@gmail.com>
2019-09-29 06:44:10 -0700

    bug fixes

--- src/javascript/api.js
+++ src/javascript/api.js
@@ -1,18 +1,18 @@
 var votes = {}
 
 // Fetch every 1000 milliseconds
-setInterval(
+setInterval(() => {
     fetch(`http://127.0.0.1:5000/votes`, { method: "GET" })
-  .then(response => {
-    return response.json();
-  })
-  .then(response => {
-    let { data } = response;
-    votes = data
-  })
-  .catch(err => {
-    console.error(err);
-  })
-, 1000);
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        let { data } = response;
+        votes = data
+      })
+      .catch(err => {
+        console.error(err);
+      })
+}, 1000);
 
 exports.votes = votes
